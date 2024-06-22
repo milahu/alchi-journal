f812a1e33a8a214be1b1b0b83a54d3d558ba0ed4
Milan Hauth <milahu@gmail.com>
2024-06-22 09:05:33 +0200

    add add-images.sh

---
+++ add-images.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eux
+
+shopt -s nullglob
+
+./move-images.sh
+
+./generate-index-files.sh
+
+git add img/*/webp
+git add html/????-??.html
+git add index.html || true
+git add ????-??.md
+
+git commit -m "add images"
+
+git status
